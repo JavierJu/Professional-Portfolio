0eeda9a8b978cb60c90286f0703efe1f41d283b7
JavierJu <boswkd@gmail.com>
2025-01-23 11:04:20 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<div align="center">
+  <img width="200" src="favicon_io/android-chrome-512x512.png" alt="Portfolio logo">
+
+# [Professional Portfolio](https://www.jacobgrisham.com/)
+![Website](https://img.shields.io/website?down_color=red&down_message=offline&up_color=brightgreen&up_message=online&url=https%3A%2F%2Fwww.jacobgrisham.com%2F)
+[![Netlify Status](https://api.netlify.com/api/v1/badges/040fac56-0e0d-4276-932e-ca308f06161c/deploy-status)](https://app.netlify.com/sites/jacobgrisham/deploys)
+![Mozilla HTTP Observatory Grade](https://img.shields.io/mozilla-observatory/grade/jacobgrisham.com)
+[![Maintainability](https://api.codeclimate.com/v1/badges/ebf5d81dbf5eb3a6318c/maintainability)](https://codeclimate.com/github/JacobGrisham/Professional-Portfolio/maintainability)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/f4ceb1f87f274625ad7369bc30246eec)](https://www.codacy.com/gh/JacobGrisham/Professional-Portfolio/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=JacobGrisham/Professional-Portfolio&amp;utm_campaign=Badge_Grade)
+![Libraries.io dependency status for GitHub repo](https://img.shields.io/librariesio/github/jacobgrisham/Professional-Portfolio) 
+</div>
+
+## 🚀 Getting Started
+To run this project locally, install dependencies using npm
+```
+$ npm install
+```
+Run node-sass in watch mode for hot reloading
+```
+$ npm run dev
+```
+Finally, copy the path for the index.html file and paste it in the browser. For example
+```
+/Users/username/Desktop/Professional-Portfolio/index.html
+```
+before pushing to production, run node-sass in production mode
+```
+$ npm run build
+```
+
+## 🔒 License
+Copyright Notice and Statement: currently [**not** offering any license](https://choosealicense.com/no-permission/). Permission only to view.
---
+++ js/common.js
@@ -0,0 +1,90 @@
+// --------------------------------------
+// Loading screen Animation
+// --------------------------------------
+setTimeout(function () {
+	document.getElementById("code-8").style.visibility = "visible";
+}, 0);
+setTimeout(function () {
+	document.getElementById("code-7").style.visibility = "visible";
+}, 500);
+setTimeout(function () {
+	document.getElementById("code-6").style.visibility = "visible";
+}, 1001);
+setTimeout(function () {
+	document.getElementById("code-5").style.visibility = "visible";
+}, 1500);
+setTimeout(function () {
+	document.getElementById("code-4").style.visibility = "visible";
+}, 2000);
+setTimeout(function () {
+	document.getElementById("code-3").style.visibility = "visible";
+}, 2500);
+setTimeout(function () {
+	document.getElementById("code-2").style.visibility = "visible";
+}, 3000);
+setTimeout(function () {
+	document.getElementById("code-1").style.visibility = "visible";
+}, 3500);
+
+// --------------------------------------
+// Loading screen logic
+// --------------------------------------
+document.onreadystatechange = () => { 
+  if (document.readyState !== "complete") { 
+      document.querySelector("body").style.visibility = "hidden"; 
+      document.querySelector(".loader").style.visibility = "visible"; 
+  } else { 
+      document.querySelector(".loader").style.display = "none"; 
+      document.querySelector("body").style.visibility = "visible"; 
+  } 
+};
+
+// --------------------------------------
+// Global site tag (gtag.js) - Google Analytics
+// --------------------------------------
+window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-SV51NLJS21');
+
+// --------------------------------------
+// Lazyloading Videos and Images
+// --------------------------------------
+const media = document.querySelectorAll("[data-src]")
+
+function preloadMedia(iframe) {
+  const src = iframe.getAttribute("data-src");
+  if(!src) {
+    return;
+  }
+  iframe.src = src;
+}
+
+const mediaOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 500px 0px"
+};
+
+const mediaObserver = new IntersectionObserver((entries, mediaObserver) => {
+  entries.forEach (entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadMedia(entry.target);
+      mediaObserver.unobserve(entry.target);
+    }
+  });
+}, mediaOptions);
+
+media.forEach(video => {
+  mediaObserver.observe(video);
+})
+
+// --------------------------------------
+// Copyright Date
+// --------------------------------------
+var date = new Date();
+var year = date.getFullYear();
+
+document.getElementById("date").innerHTML = year;
